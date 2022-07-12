3e891f1cc74ca102d6ff5292becd3e676eb737e8
mabobakr <m.abobakr10@yahoo.com>
2022-07-12 16:22:07 +0200

    modified data splitting and files structure

--- preprocessing.py
+++ preprocessing.py
@@ -10,13 +10,9 @@ from joblib import dump, load
 
 
 
-def read_file(fileno):
-  i = fileno
-  x = np.load(f"numpy_data/A0{i}TX.npy")
-  x = x[:, :,1:]
-  x = np.swapaxes(x, 1, 2)
-  y = np.load(f"numpy_data/A0{i}TY.npy")
-
+def read_file(directory, filename):
+  x = np.load(f"{directory}/{filename}X.npy")
+  y = np.load(f"{directory}/{filename}Y.npy")
   return x, y
 
 # fileno is the number of subect
@@ -30,10 +26,15 @@ def read_file_sim(fileno): # For Simulator
 
   return x, y
 
+
 # X is data of one sample
 # model_name is the joblib file name of model
 # csp_name is the joblib file name of the csp list
-def predict(x, model, csp):
+model = load("model.joblib")
+csp = load("csp.joblib")
+def predict(x):
+  global model
+  global csp
   labels = {1: "L", 2: "R", 3: "F", 4: "B"}
   x = [x]
   # model = load(model_name)
@@ -44,28 +45,11 @@ def predict(x, model, csp):
   X_test_f = np.concatenate(tuple(csp[j].transform(test_coeff[j]) for j in range(coeff_len)), axis=-1)
   return labels[model.predict(X_test_f[0:1])[0]]
 
-# X is data of one sample
-# model_name is the joblib file name of model
-# csp_name is the joblib file name of the csp list
-model = load("model.joblib")
-csp = load("csp.joblib")
-def efficient_predict(x):
-  global model
-  global csp
-  labels = {1: "L", 2: "R ", 3: "F", 4: "B"}
-  x = [x]
-  # model = load(model_name)
-  # csp = load(csp_name)
-  test_coeff = featurize(x)
-  coeff_len = len(test_coeff)
-  
-  X_test_f = np.concatenate(tuple(csp[j].transform(test_coeff[j]) for j in range(coeff_len)), axis=-1)
-  return labels[model.predict(X_test_f[0:1])[0]]
 
+# apply discrete wavelet transform
 def featurize(x):
   coeff = pywt.wavedec(x, 'db4', level = 7)
-  return coeff
-  
+  return coeff  
 
 
 # Apply butterworth filter
@@ -75,54 +59,20 @@ def filter(signal, band, fs):
   filtered = sosfilt(sos, signal)
   return filtered
 
-# Extract EEG training data and convert it to numpy
-def convert_file(filename, test=False):
-  
+
+def read_gdf(filename):
   # load the gdf file
   data = mne.io.read_raw_gdf(filename)
 
-  # get data in dataframe format
-  dataframe = data.to_data_frame()
+  # get data samples shape (num_of_samples, num_electrodes)
+  values = data.to_data_frame().values[:, 1:]
 
   # Get the events 
   events = mne.events_from_annotations(data)
   codes = events[1]
   events = events[0]
-  # print(events, codes)
-  # for ev in events:
-  #   print(ev)
 
-  # convert annotations to mne codes
-  if test:
-    cfilter = np.asarray(['783'])
-  else:
-    cfilter = np.asarray(['769', '770', '771', '772'])
-  lis = np.asarray([codes[i] for i in cfilter])
-
-  # filter for classes
-  ev = events[np.in1d(events[:, 2], lis)]
-
-  # extract the samples from events
-  x = np.zeros((288, 750, 26))
-  y = np.zeros(288)
-
-  values = dataframe.values#filter(dataframe.values.T, [8, 12], 250).T
-  
-  for point in range(len(ev)):
-    x[point] = values[ev[point][0]:ev[point][0]+750]
-    y[point] = ev[point][2] - lis[0] + 1
-    
-  # Create directory for numpy data
-  if not os.path.exists("numpy_data"):
-    os.mkdir(os.path.join(os.getcwd(), "numpy_data"))
-
-  # get file name without extension or path
-  new_name = os.path.splitext(os.path.basename(filename))[0]
-  
-  # Save data to numpy arrays
-  np.save(f"numpy_data/{new_name}X", x)
-  if not test:
-    np.save(f"numpy_data/{new_name}Y", y)
+  return values, codes, events
 
 
 # Apply convert file to all files in the data directory
@@ -135,27 +85,119 @@ def convert_data():
     exit(1)
 
   for file in datafiles:
-    if re.match(r"A0[0-9]T.gdf", file):
-      convert_file("data/" +file)
-    if re.match(r"A0[0-9]E.gdf", file):
-      convert_file("data/" +file, True)
+    if re.match(r"A0[0-9].gdf", file):
+      
+      values, codes, events = read_gdf("data/" + file)
+
+      # number of samples of one electrode 
+      psize = 750
+
+      # Extract idle data points from raw data
+      x_idle, y_idle = idle_points(values, events, psize)
+      xi_train, xi_test, yi_train, yi_test = train_test_split(x_idle, y_idle, random_state = 42, test_size = 0.2)
+      
+      # Extract actions data points from raw data
+      x_actions, y_actions = action_points(values, codes, events, psize)
+      xa_train, xa_test, ya_train, ya_test = train_test_split(x_actions, y_actions, random_state = 42, test_size = 0.2)
+
+      # xa => x of actions model, xi => x of idle model
+
+      # get file name without extension or path
+      new_name = os.path.splitext(os.path.basename(file))[0]
+
+        # Create directory for numpy data
+      dir_name = "action_train"
+      if not os.path.exists(dir_name):
+        os.mkdir(os.path.join(os.getcwd(), dir_name))
+      
+      # save X_actions and y_actions train
+      np.save(f"{dir_name}/{new_name}X", xa_train)
+      np.save(f"{dir_name}/{new_name}Y", ya_train)
+
+      dir_name = "action_test"
+      if not os.path.exists(dir_name):
+        os.mkdir(os.path.join(os.getcwd(), dir_name))
+      
+      # save X_action and y_actions test
+      np.save(f"{dir_name}/{new_name}X", xa_test)
+      np.save(f"{dir_name}/{new_name}Y", ya_test)
+
+      dir_name = "idle_train"
+      if not os.path.exists(dir_name):
+        os.mkdir(os.path.join(os.getcwd(), dir_name))
+      
+      # save (X_actions train + X_idle train) and (y_actions train + y_idle train)
+      x = np.concatenate((xa_train, xi_train))
+      y = np.concatenate((ya_train, yi_train))
+      np.save(f"{dir_name}/{new_name}X", x)
+      np.save(f"{dir_name}/{new_name}Y", y)
+
+      dir_name = "idle_test"
+      if not os.path.exists(dir_name):
+        os.mkdir(os.path.join(os.getcwd(), dir_name))
+
+      # save (X_actions test + X_idle test) and (y_actions test + y_idle test)
+      x = np.concatenate((xa_test, xi_test))
+      y = np.concatenate((ya_test, yi_test))
+      np.save(f"{dir_name}/{new_name}X", x)
+      np.save(f"{dir_name}/{new_name}Y", y)
 
 
-# Splits the data and save test data in a directory
-def create_test_data():
-  if not os.path.exists("numpy_test_data"):
-    print("Please create a folder named numpy_test_data and rerun")
-    print("note: you should run convert_data first")
-    exit(1)
+
+# Extract idle data points from raw data
+# psize is the number of samples of one data point (for one electrode)
+def idle_points(values, events, psize = 750):
+  
+  # last idle sample
+  idle_size = events[6, 0]
+  
+  # number of data points
+  num_idle_samples = idle_size // psize
+  
+  # number of samples of all idle points
+  idle_size = num_idle_samples * psize
+
+  # x and y arrays of size 288(actions) + number of idle samples
+  x = np.zeros((num_idle_samples, psize, 25))
+  y = np.zeros(num_idle_samples)
+  
+
+  index = 0
+  for i in range(0, idle_size, psize):
+    x[index] = values[i:i + psize]
+    y[index] = 0
+    index += 1
+  
+  x = np.swapaxes(x, 1, 2)
+
+  return x, y
 
 
-  for i in range(1, 10):
-    x, y = read_file(i)
+def action_points(values, codes, events, psize = 750):
+  
+  # convert annotations to mne codes
+  cfilter = np.asarray(['769', '770', '771', '772'])
+  lis = np.asarray([codes[i] for i in cfilter])
 
-    X_train, X_test, y_train, y_test = train_test_split(x, y, random_state = 100, test_size = 0.2)
+  # filter for classes
+  ev = events[np.in1d(events[:, 2], lis)]
 
-    # Save data to numpy arrays
-    np.save(f"numpy_test_data/{i}X", X_test)
-    np.save(f"numpy_test_data/{i}Y", y_test)
+  # extract the samples from events
+  x = np.zeros((288, psize, 25))
+  y = np.zeros(288)
+
+  
+  for point in range(len(ev)):
+    x[point] = values[ev[point][0]:ev[point][0]+psize]
+    y[point] = ev[point][2] - lis[0] + 1
+    
+  x = np.swapaxes(x, 1, 2)
+
+  return x, y
+  
+
+
+  
+
 
 
